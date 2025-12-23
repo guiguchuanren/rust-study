32f66b93f9f53de794fcabff7b8b07eb58e77817
GaoHaiChao <1742025930@qq.com>
2025-12-23 18:27:14 +0800

    清理多余文件

--- day01/target/debug/day01.d
+++
@@ -1,1 +0,0 @@
-D:\rustStudy\day01\target\debug\day01.exe: D:\rustStudy\day01\src\main.rs
--- day01/target/debug/deps/day01.d
+++
@@ -1,5 +0,0 @@
-D:\rustStudy\day01\target\debug\deps\day01.d: src\main.rs
-
-D:\rustStudy\day01\target\debug\deps\day01.exe: src\main.rs
-
-src\main.rs:
